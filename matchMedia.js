826dcd2cd458213d121d5ebb1294909b6307eef7
Zoltan <zkubanyi@gmail.com>
2017-12-05 13:12:45 -0800

    Still trying to get the resize to work triggered on overflow.

--- matchMedia.js
+++ matchMedia.js
@@ -1,14 +1,19 @@
+
+
 /* Event handler */
 if (matchMedia) {
-	const mq = window.matchMedia("(min-width: 500px)");
-	mq.addListener(WidthChange);
-	WidthChange(mq);
+    const mq = window.matchMedia("(min-width: 500px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
 }
 
 // event listener
 function WidthChange(mq) {
 
-	const msg = (mq.matches ? "more" : "less") + " than 500 pixels";
-	document.getElementById("#current").firstChild.nodeValue = msg;
+    const msg = (mq.matches ? "more" : "less") + " than 500 pixels";
 
-}
+    //var allIDs = document.querySelectorAll("[id]");
+    //var allID = $('[id]');
+    document.getElementById("current").firstChild.nodeValue = msg;
+
+})
