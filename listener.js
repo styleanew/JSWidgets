f9acbff35ff5c47cfa434236c1be6bbb5b04e3d2
Zoltan <zkubanyi@gmail.com>
2017-12-05 17:10:35 -0800

    Finally I have the div resizing as responsive as I wanted. Just used a JavaScript global variable to save the width of the window at the time of the change of the child from absolute postion to relative. And then resestting column count and position once the width reaches that size.

---
+++ listener.js
@@ -0,0 +1,16 @@
+
+
+document.getElementById("detailsMain").addEventListener("onchange", OverflowResponse);
+
+function OverflowResponse() {
+	var newHeight = document.getElementById("detailsMain");
+	
+}
+
+
+document.getElementById("fname").addEventListener("change", myFunction);
+
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
